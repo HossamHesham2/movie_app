c792e87c0784adc190c9c7c677766a7f1a687330
Hossam <hossamhesham413@gmail.com>
2025-08-20 18:42:48 +0300

    done splash screen

--- lib/core/routes/routes_manager.dart
+++ lib/core/routes/routes_manager.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/cupertino.dart';
+import 'package:movie_app/Views/login_view/login_view.dart';
 import 'package:movie_app/Views/main_layout/main_layout_view.dart';
 
 class RoutesManager {
   static const String mainLayout = "/";
+  static const String loginView = "/loginView";
 
   static Route? getRoutes(RouteSettings settings) {
     switch (settings.name) {
       case mainLayout:
         return CupertinoPageRoute(builder: (context) => MainLayoutView());
+      case loginView:
+        return CupertinoPageRoute(builder: (context) => LoginView());
     }
     return null;
   }
